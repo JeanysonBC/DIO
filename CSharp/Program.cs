61baffcb334ca03bef2a56eeb5a45b9df548c51d
Jeanyson Campos <jeanysonbc@gmail.com>
2023-05-08 19:06:09 -0300

    Atualização da prática com exemplos - curso C# DIO.

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,18 +1,143 @@
 ﻿using CSharp.Models;
 
-Calculadora calc = new Calculadora();
+List<string> listaString = new List<string>();
 
-// calc.Somar(4, 6);
-// calc.Subtrair(10, 3);   
-// calc.Mutiplicar(3,40);
-// calc.Dividir(150,5);
-// calc.Potencia(9,3);
+listaString.Add("Maranhão");
+listaString.Add("Piauí");
+listaString.Add("Pará");
 
-// calc.Seno(30);
-// calc.Cosseno(30);
-// calc.Tangente(30);
+for(int contador = 0; contador < listaString.Count; contador++)
+{
+    
+    Console.WriteLine($"Posição Nº {contador} = {listaString[contador]}");
+}
 
-calc.RaizQuadrada(4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int[] arrayInteiros = new int [3];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 43;
+// arrayInteiros[2] = 54;
+
+
+// int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+
+// Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+
+
+// // Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+
+// for(int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Posição Nº {contador} = {arrayInteiros[contador]}");
+// }
+
+// int cont = 0;
+// foreach(int valor in arrayInteiros)
+// {
+//     Console.WriteLine($"Posição Nº {cont} = {valor}");
+//     cont++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// string opcao;
+
+// while(true)
+// {
+//     Console.WriteLine("Digite a sua opção:");
+//     Console.WriteLine("1 - Cadastrar cliente");
+//     Console.WriteLine("2 - Buscar cliente");
+//     Console.WriteLine("3 - Apagar cliente");
+//     Console.WriteLine("4 - Encerrar");
+
+//     opcao = Console.ReadLine();
+
+//     switch(opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro de cliente");
+//             break;
+
+//         case "2":
+//             Console.WriteLine("Busca de cliente");
+//             break;
+        
+//         case "3":
+//             Console.WriteLine("Apagar de cliente");
+//             break;
+
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             Environment.Exit(0);
+//             break;
+
+//         default:
+//             Console.WriteLine("Opção inválida");
+//             break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Calculadora calc = new Calculadora();
+
+// // calc.Somar(4, 6);
+// // calc.Subtrair(10, 3);   
+// // calc.Mutiplicar(3,40);
+// // calc.Dividir(150,5);
+// // calc.Potencia(9,3);
+
+// // calc.Seno(30);
+// // calc.Cosseno(30);
+// // calc.Tangente(30);
+
+// calc.RaizQuadrada(4);
 
 
 
---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,36 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "jean",sobrenome: "campos");
+Pessoa p2 = new Pessoa(nome: "jana",sobrenome: "neves sa");
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// p1.Nome = "Jean";
+// p1.Sobrenome = "Campos";
+// p1.Idade = 30;
+// p1.Apresentar();
