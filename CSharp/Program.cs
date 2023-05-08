533de757ffbc9785f87639e05df52a940e8cda22
Jeanyson Campos <jeanysonbc@gmail.com>
2023-05-08 18:58:33 -0300

    Exemplos aula de métodos C# - DIO

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,7 +1,32 @@
-﻿
-DateTime dataAtual = DateTime.Now;
+﻿using CSharp.Models;
 
-Console.WriteLine(dataAtual);   
+Calculadora calc = new Calculadora();
+
+// calc.Somar(4, 6);
+// calc.Subtrair(10, 3);   
+// calc.Mutiplicar(3,40);
+// calc.Dividir(150,5);
+// calc.Potencia(9,3);
+
+// calc.Seno(30);
+// calc.Cosseno(30);
+// calc.Tangente(30);
+
+calc.RaizQuadrada(4);
+
+
+
+
+
+
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now;
+
+// Console.WriteLine(dataAtual);   
 
 
 
